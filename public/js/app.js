dd7a420c7560fcba9926b7e0b7848d3f9a66ce31
yairhub <yairkiel7@gmail.com>
2019-06-12 20:36:04 +0300

    exdb

--- public/js/app.js
+++ public/js/app.js
@@ -84202,8 +84202,8 @@ function validateProperty(_ref, sch) {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\xampp\htdocs\laravel_react\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\xampp\htdocs\laravel_react\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\xampp\htdocs\laravel-react\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\xampp\htdocs\laravel-react\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
